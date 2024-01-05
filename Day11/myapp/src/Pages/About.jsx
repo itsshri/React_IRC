cd908416d2b585aae6322548a07e079962d8ef75
itsshri <shrijithr2004@gmail.com>
2024-01-06 00:57:39 +0530

    Day11

---
+++ Day11/myapp/src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>Shrijith R</h1>
+        <h2>CSE C</h2>                         
+    </div>
+  )
+}
+
+export default About;
---
+++ Day11/myapp/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Sri Krishna College Of Technology</h1>
+        <p>REACT_IRC</p>
+    </div>
+  )
+}
+
+export default Home;
